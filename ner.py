07de4eacb95717e1b37a2a8416bd875ff3e95eae
Dwi Aji Kurniawan <dwi.aji.kurniawan@gmail.com>
2017-04-01 15:05:40 +0700

    Final commit (perhaps)

--- ner.py
+++ ner.py
@@ -5,12 +5,12 @@ import re
 
 import nltk
 from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
-#from nltk.stem import WordNetLemmatizer
+#from nltk.stem.snowball import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
 
 english_stopwords = set(stopwords.words('english'))
-stemmer = SnowballStemmer("english")
-#lemmatizer = WordNetLemmatizer()
+#stemmer = SnowballStemmer("english")
+lemmatizer = WordNetLemmatizer()
 
 pos_path = 'Movie Reviews Data Set/review_polarity/txt_sentoken/pos'
 neg_path = 'Movie Reviews Data Set/review_polarity/txt_sentoken/neg'
@@ -21,16 +21,12 @@ neg_files = [join(neg_path, f) for f in listdir(neg_path) if isfile(join(neg_pat
 pos_words = {}
 neg_words = {}
 
-pos_df = {}
-neg_df = {}
-
 for file_path in pos_files:
     with open(file_path) as f:
-        df = {}
         for line in f:
             line = re.sub(r'\s[^a-zA-Z0-9]',' ', line) 
             tokens = line.split()
-            tokens = [stemmer.stem(token).encode("ascii") \
+            tokens = [lemmatizer.lemmatize(token).encode("ascii") \
                 for token in tokens \
                 if '\'s' not in token \
                 and '\'t' not in token]
@@ -39,28 +35,17 @@ for file_path in pos_files:
                 #token = stemmer.stem(token)
                 #token = lemmatizer.lemmatize(token).encode("ascii")
                 if 'NN' in token[1] and token[0] not in english_stopwords:
-                    try:
-                        df[token] += 1
-                    except:
-                        df[token] = 1
                     try:
                         pos_words[token] += 1
                     except:
                         pos_words[token] = 1
-        for key in df:
-            try:
-                pos_df[key] += 1
-            except:
-                pos_df[key] = 1
-            
 
 for file_path in neg_files:
     with open(file_path) as f:
-        df = {}
         for line in f:
             line = re.sub(r'\s[^a-zA-Z0-9]',' ', line) 
             tokens = line.split()
-            tokens = [stemmer.stem(token).encode("ascii") \
+            tokens = [lemmatizer.lemmatize(token).encode("ascii") \
                 for token in tokens \
                 if '\'s' not in token \
                 and '\'t' not in token]
@@ -70,24 +55,12 @@ for file_path in neg_files:
                 #token = lemmatizer.lemmatize(token).encode("ascii")
                 if 'NN' in token[1] and token[0] not in english_stopwords:
                     try:
-                        df[token] += 1
-                    except:
-                        df[token] = 1
-                    try:
                         neg_words[token] += 1
                     except:
                         neg_words[token] = 1
-        for key in df:
-            try:
-                neg_df[key] += 1
-            except:
-                neg_df[key] = 1
 
 pos_words = sorted(pos_words.items(), key=operator.itemgetter(1), reverse=True)
 neg_words = sorted(neg_words.items(), key=operator.itemgetter(1), reverse=True)
-
-pos_df = sorted(pos_df.items(), key=operator.itemgetter(1), reverse=True)
-neg_df = sorted(neg_df.items(), key=operator.itemgetter(1), reverse=True)
 
 with open('pos.csv', 'a') as f:
     for item in pos_words:
@@ -103,22 +76,6 @@ for word in pos_words[:40]:
 
 print 'Total neg words:', len(neg_words)
 for word in neg_words[:40]:
-    print word
-
-with open('pos_df.csv', 'a') as f:
-    for item in pos_df:
-        f.write('{0},{1},{2}\n'.format(item[0][0], item[0][1], item[1]))
-
-with open('neg_df.csv', 'a') as f:
-    for item in neg_df:
-        f.write('{0},{1},{2}\n'.format(item[0][0], item[0][1], item[1]))
-
-print 'IDF pos'
-for word in pos_df[:40]:
-    print word
-
-print 'IDF neg'
-for word in neg_df[:40]:
     print word
 #print pos_words[:30]
 
